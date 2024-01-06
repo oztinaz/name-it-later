a7c7375f97f64676b52e88acdf55dbb5f3a8cd87
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-01-06 14:54:07 +0300

    Add authorization views

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 // Main Views
 import HomeView from '@/views/HomeView.vue'
 
+// Spotify
+// Authorization
+import SpotifyAskAuthorizationView from '@/views/spotify/AskAuthorizationView.vue'
+import SpotifyAuthorizationView from '@/views/spotify/AuthorizationView.vue'
+import SpotifyMainView from '@/views/spotify/MainView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,6 +16,23 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/spotify',
+      name: 'spotify',  
+      component: SpotifyMainView,
+      children: [
+        {
+          path: 'ask-authorization',
+          name: 'spotify-ask-authorization',
+          component: SpotifyAskAuthorizationView
+        },
+        {
+          path: 'authorization',
+          name: 'spotify-authorization',
+          component: SpotifyAuthorizationView
+        }
+      ]
     }
   ]
 })
