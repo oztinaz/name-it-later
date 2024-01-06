dcee04262201b185398c0c60219bfc21aec320f6
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-01-06 15:20:20 +0300

    Add SpotifyAccessToken type

---
+++ src/types/spotify/Token.ts
@@ -0,0 +1,10 @@
+export type SpotifyToken = {
+  access_token: string
+  token_type: string
+  scope: string
+  expires_in: number
+}
+
+export type SpotifyAccessToken = SpotifyToken & {
+  refresh_token: string
+}
