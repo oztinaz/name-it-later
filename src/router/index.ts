db1c9cf8c459d52a5c866b3c460cf57a392499fb
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-01-06 17:38:34 +0300

    Redirect if not authorized

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useSpotifyTokenStore } from '@/stores/spotify/token'
+import { CookieUtils } from '@/utils/Cookie'
+import type { SpotifyRefreshToken } from '@/types/spotify/Token'
 
 // Main Views
 import HomeView from '@/views/HomeView.vue'
@@ -35,6 +38,39 @@ const router = createRouter({
       ]
     }
   ]
+})
+
+router.beforeEach(async (to, from) => {
+  const accessToken: string | undefined = CookieUtils.get('X-SPOTIFY-ACCESS-TOKEN')
+  const refreshToken: string | undefined = CookieUtils.get('X-SPOTIFY-REFRESH-TOKEN')
+
+  if (
+    accessToken === undefined &&
+    refreshToken === undefined &&
+    to.path.startsWith('/spotify') &&
+    to.name !== 'spotify-ask-authorization' &&
+    to.name !== 'spotify-authorization'
+  ) {
+    return {
+      name: 'spotify-ask-authorization'
+    }
+  }
+
+  if (
+    accessToken === undefined &&
+    refreshToken !== undefined &&
+    to.path.startsWith('/spotify')
+  ) {
+    const spotifyTokenStore = useSpotifyTokenStore()
+    const { fetchRefreshToken } = spotifyTokenStore
+    const spotifyRefreshToken: SpotifyRefreshToken = await fetchRefreshToken(refreshToken)
+    CookieUtils.remove('X-SPOTIFY-REFRESH-TOKEN')
+
+    const expiresAt = new Date()
+    expiresAt.setTime(expiresAt.getTime() + spotifyRefreshToken.expires_in * 1000)
+
+    CookieUtils.set('X-SPOTIFY-ACCESS-TOKEN', spotifyRefreshToken.access_token, expiresAt)
+  }
 })
 
 export default router
