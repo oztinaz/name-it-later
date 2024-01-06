695ec0481e76d6b72a8b6ed016cebed6709cfe9e
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-01-06 14:54:32 +0300

    Lint and format

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ const router = createRouter({
     },
     {
       path: '/spotify',
-      name: 'spotify',  
+      name: 'spotify',
       component: SpotifyMainView,
       children: [
         {
