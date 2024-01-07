7da3e7ec64d2acb495092bfb17fff58daf315db4
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-01-07 15:39:24 +0300

    Implement SearchRequest

---
+++ src/models/spotify/requests/api/SearchRequest.ts
@@ -0,0 +1,41 @@
+import { AbstractSpotifyApiGetRequest } from '@/models/spotify/requests/api/AbstractApiGetRequest'
+import type { SpotifySearchItem } from '@/types/spotify/SearchItem'
+import type { SpotifyBasicAlbum } from "@/types/spotify/Album"
+import type { SpotifyArtist } from "@/types/spotify/Artist"
+import type { SpotifyTrack } from "@/types/spotify/Track"
+
+export class SearchRequest<T extends SpotifyBasicAlbum | SpotifyArtist | SpotifyTrack> extends AbstractSpotifyApiGetRequest<SpotifySearchItem<T>> {
+  private urlPath: string = '/search'
+
+  private query: string = ''
+
+  private type: 'album' | 'artist' | 'track'
+  
+  constructor(type: 'album' | 'artist' | 'track') {
+    super()
+    this.type = type
+  }
+
+  public getUrlPath(): string {
+    return this.urlPath
+  }
+
+  public getQuery(): string {
+    return this.query
+  }
+
+  public setQuery(query: string): void {
+    this.query = query
+  }
+
+  public getType(): string {
+    return this.type
+  }
+
+  public getRequestParams(): object {
+    return {
+      q: this.getQuery(),
+      type: this.getType()
+    }
+  }
+}
