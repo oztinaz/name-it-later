ce21a905034cce82bc36e8d83c5b14db472c2a15
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-01-09 17:53:41 +0300

    Update seed views

--- src/views/spotify/create-playlist/seeds/GenresView.vue
+++ src/views/spotify/create-playlist/seeds/GenresView.vue
@@ -1,8 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Seed from '@/components/spotify/seed/Seed.vue';
+import { GetGenresRequest } from '@/models/spotify/requests/api/GetGenresRequest';
+import { useSpotifyGenreStore } from '@/stores/spotify/genre';
+import type { SpotifyGenre } from '@/types/spotify/Genre';
+import type { SpotifyGetGenreResponse } from '@/types/spotify/responses/GetGenresResponse';
+import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue';
+
+const spotifyGenreStore = useSpotifyGenreStore()
+const { genres } = storeToRefs(spotifyGenreStore)
+const { fetchGenres, setGenres } = spotifyGenreStore
+
+const type = 'genre'
+
+const mapper = (genres: SpotifyGenre[]) => {
+  return genres.map((genre: SpotifyGenre) => {
+    return {
+      id: genre,
+      description: makeDescription(genre),
+      imageUrl: getImageUrl(genre),
+      title: genre
+    }
+  })
+}
+
+const retriever = (): SpotifyGenre[] => {
+  return genres.value.filter((genre: SpotifyGenre) => genre.toLocaleLowerCase().includes(genre.toLocaleLowerCase()))
+}
+
+const getImageUrl = (genre: SpotifyGenre): string => {
+  return ''
+}
+
+const makeDescription = (genre: SpotifyGenre): string => {
+  return ''
+}
+
+onMounted(async () => {
+  const response: SpotifyGetGenreResponse = await fetchGenres()
+  setGenres(response.genres)
+})
+</script>
 
 <template>
   <div id="spotify-create-playlist-seeds-genres-view">
-    Genres
+    <Seed :type="type" :mapper="mapper" :retriever="retriever"/>
   </div>
 </template>
 
--- src/views/spotify/create-playlist/seeds/TracksView.vue
+++ src/views/spotify/create-playlist/seeds/TracksView.vue
@@ -1,8 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Seed from '@/components/spotify/seed/Seed.vue';
+import { SearchRequest } from '@/models/spotify/requests/api/SearchRequest';
+import { useSpotifySearchStore } from '@/stores/spotify/search';
+import type { SpotifyBasicArtist } from '@/types/spotify/Artist';
+import type { SpotifySearchItem } from '@/types/spotify/responses/SearchItem';
+import type { SpotifyTrack } from '@/types/spotify/Track';
+
+const spotifySearchStore = useSpotifySearchStore()
+const { search } = spotifySearchStore
+
+const type = 'track'
+const request = new SearchRequest<SpotifyTrack>(type)
+
+const mapper = (tracks: SpotifyTrack[]) => {
+  return tracks.map((track: SpotifyTrack) => {
+    return {
+      id: track.id,
+      description: makeDescription(track),
+      imageUrl: getImageUrl(track),
+      title: track.name
+    }
+  })
+}
+
+const retriever = async (request: SearchRequest<SpotifyTrack>): Promise<SpotifyTrack[]> => {
+  const response: SpotifySearchItem<SpotifyTrack> = await search<SpotifyTrack>(request)
+  if (response.tracks) {
+    return response.tracks.items
+  }
+  return []
+}
+
+const getImageUrl = (track: SpotifyTrack): string => {
+  return track.album.images[0] ? track.album.images[0].url : ''
+}
+
+const makeDescription = (track: SpotifyTrack): string => {
+  return `by ${track.album.artists.map((artist: SpotifyBasicArtist) => artist.name).join(', ')}`
+}
+</script>
 
 <template>
   <div id="spotify-create-playlist-seeds-tracks-view">
-    Tracks
+    <Seed :type="type" :mapper="mapper" :retriever="retriever" :request="request"/>
   </div>
 </template>
 
